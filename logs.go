5003f9361f2f315deeff82300b024d874d964c1c
Antonin Portelli <antonin.portelli@me.com>
2022-12-14 14:54:13 +0000

    Added 2 channels and test program

--- logs.go
+++ logs.go
@@ -27,24 +27,36 @@ import (
 var Dbg = Logger{
 	Level:     2,
 	Color:     color.New(color.FgYellow),
-	StdLogger: log.New(os.Stdout, "DEBUG: ", 0),
+	StdLogger: log.New(os.Stdout, "DEBUG   : ", 0),
+}
+
+var Warn = Logger{
+	Level:     2,
+	Color:     color.New(color.FgHiMagenta, color.Bold),
+	StdLogger: log.New(os.Stderr, "WARNING : ", 0),
 }
 
 var Err = Logger{
 	Level:     0,
 	Color:     color.New(color.FgRed, color.Bold),
-	StdLogger: log.New(os.Stderr, "ERROR: ", 0),
+	StdLogger: log.New(os.Stderr, "ERROR   : ", 0),
 }
 
 var Inf = Logger{
 	Level:     1,
 	Color:     color.New(color.FgHiBlack),
-	StdLogger: log.New(os.Stdout, " INFO: ", 0),
+	StdLogger: log.New(os.Stdout, "INFO    : ", 0),
 }
 
 var Msg = Logger{
 	Level:     0,
 	Color:     color.New(),
+	StdLogger: log.New(os.Stdout, "", 0),
+}
+
+var Emph = Logger{
+	Level:     0,
+	Color:     color.New(color.FgGreen, color.Bold),
 	StdLogger: log.New(os.Stdout, "", 0),
 }
 
